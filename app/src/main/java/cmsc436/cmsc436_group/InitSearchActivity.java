49ed1e4a26f991db5b1fc63d33be768da4871ba5
Rebecca Chapin-Ridgely <rchapin@terpmail.umd.edu>
2017-11-19 02:05:19 -0500

    Initial commit with nothing special

---
+++ app/src/main/java/cmsc436/cmsc436_group/InitSearchActivity.java
@@ -0,0 +1,13 @@
+package cmsc436.cmsc436_group;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init_search);
+    }
+}

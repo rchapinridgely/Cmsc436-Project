03b08a60f0be1395282daae4b94e74daea4198a8
Seung Yeop Lee <yeopdodo86@users.noreply.github.com>
2017-12-02 14:40:08 -0500

    lat and long with address or zip, not completed
    sorry commit 4 was committed in the wrong place
    I am doing the same file again we need to delete the src folder in the root

---
+++ app/src/main/java/cmsc436group/myapplication/MapScreen.java
@@ -0,0 +1,34 @@
+package cmsc436group.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MapScreen extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button NVDIButton = (Button)findViewById(R.id.NVDI);
+        NVDIButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent submitIntent = new Intent(this, SummaryActivity.class);
+               // startActivity(new Intent(MapScreen.this, .class));
+            }
+        });
+
+        Button NAVButton = (Button)findViewById(R.id.NAV);
+        NAVButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent submitIntent = new Intent(this, SummaryActivity.class);
+                //startActivity(new Intent(MapScreen.this, .class));
+            }
+        });
+    }
+}
